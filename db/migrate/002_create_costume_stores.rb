9a8e2233bb5fbaf37c559aecf47f2293dcf95ee6
Heather Kirk <hskirk@gmail.com>
2019-04-09 01:24:32 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,6 +5,10 @@ class CreateCostumesStores < ActiveRecord::Migration[5.1]
   def change
     create_table :costumestore do |t|
       t.string :name 
-      
+      t.string :location
+      t.string :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t
   end 
 end 
