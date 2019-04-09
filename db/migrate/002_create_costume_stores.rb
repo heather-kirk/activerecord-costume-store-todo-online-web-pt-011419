45d10c4c98f9bd76c0fe410faf11c4b0a0fbf43b
Heather Kirk <hskirk@gmail.com>
2019-04-09 01:23:28 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,10 @@
 # Create your costume_stores migration here
+
+class CreateCostumesStores < ActiveRecord::Migration[5.1]
+  
+  def change
+    create_table :costumestore do |t|
+      t.string :name 
+      
+  end 
+end 
